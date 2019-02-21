61c9b2af854c5e21e8050e570013e5b133a4e1f8
knromaric <nzekeng@gmail.com>
2019-02-21 13:04:34 +0200

    Added persistence of settings to localStorage

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -6,19 +6,35 @@ import {
 
 
 export const setDisableBalanceOnAdd=()=>{
+  const settings = toggleState('disableBalanceOnAdd');
   return{
-    type: DISABLE_BALANCE_ON_ADD
+    type: DISABLE_BALANCE_ON_ADD,
+    payload: settings.disableBalanceOnAdd
   }
 }
 
 export const setDisableBalanceOnEdit=()=>{
+  const settings = toggleState('disableBalanceOnEdit');
   return{
-    type: DISABLE_BALANCE_ON_EDIT
+    type: DISABLE_BALANCE_ON_EDIT,
+    payload: settings.disableBalanceOnEdit
   }
 }
 
 export const setAllowRegistration=()=>{
+  const settings = toggleState('allowRegistration');
   return{
-    type: ALLOW_REGISTRATION
+    type: ALLOW_REGISTRATION,
+    payload: settings.allowRegistration
   }
+}
+
+function toggleState(state) {
+  //get settings from localstorage
+  const settings = JSON.parse(localStorage.getItem('settings'));
+  //toggle
+  settings[state] = !settings[state];
+  //set back to localstorage
+  localStorage.setItem('settings', JSON.stringify(settings));
+  return settings;
 }
--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
-import {createStore, combineReducers, compose} from 'redux';
+import {
+  createStore,
+  combineReducers,
+  compose
+} from 'redux';
 import firebase from 'firebase';
 import 'firebase/firestore';
-import {reactReduxFirebase, firebaseReducer} from 'react-redux-firebase';
-import {reduxFirestore, firestoreReducer} from 'redux-firestore';
+import {
+  reactReduxFirebase,
+  firebaseReducer
+} from 'react-redux-firebase';
+import {
+  reduxFirestore,
+  firestoreReducer
+} from 'redux-firestore';
 //Reducers
 import notifyReducer from './reducers/notifyReducer'
 import settingsReducer from './reducers/settingsReducer'
@@ -20,7 +30,7 @@ const firebaseConfig = {
 //react-redux-firebase config
 const rrfConfig = {
   userProfile: 'users',
-  useFirestoreForProfile: true 
+  useFirestoreForProfile: true
 }
 
 //initialize firebase instance
@@ -32,25 +42,39 @@ firebase.initializeApp(firebaseConfig);
 
 // Add reactReduxFirebase enhancer when making store creator
 const createStoreWithFirebase = compose(
-  reactReduxFirebase(firebase, rrfConfig), 
-  reduxFirestore(firebase) 
+  reactReduxFirebase(firebase, rrfConfig),
+  reduxFirestore(firebase)
 )(createStore)
 
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer ,
+  firestore: firestoreReducer,
   notify: notifyReducer,
   settings: settingsReducer
 })
 
+//check for settings in localstorage
+if (!localStorage.getItem('settings')) {
+  //default settings
+  const defaultSettings = {
+    disableBalanceOnAdd: true,
+    disableBalanceOnEdit: false,
+    allowRegistration: false
+  }
+  // set to localstorage
+  localStorage.setItem('settings', JSON.stringify(defaultSettings));
+}
+
 //create our initial state
-const initialState = {};
+const initialState = {
+  settings: JSON.parse(localStorage.getItem('settings'))
+};
 
 //create our store
 const store = createStoreWithFirebase(rootReducer, initialState, compose(
   reactReduxFirebase(firebase),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && 
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__()
-)); 
+));
 
 export default store;
